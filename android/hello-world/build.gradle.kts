77fc89f41c13f2ab7dbd80987d46dc0cb9dc6788
linuxificator <linuxificator@gmail.com>
2026-08-22 21:47:19 +0200

    Add Android AMY hello-world app module

---
+++ android/hello-world/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "org.amy.hello"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "org.amy.hello"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "x86_64")
+        }
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":amy-service"))
+}

e62a9cd088dae3d715399d72e027a71035f60f87
linuxificator <linuxificator@gmail.com>
2026-08-23 12:25:37 +0200

    Fix Godot Android AAR packaging and ABI-specific exports

--- godot/android-hello-world/prepare.sh
+++ godot/android-hello-world/prepare.sh
@@ -7,13 +7,15 @@ GRADLE_BIN="${GRADLE_BIN:-gradle}"
 
 (
   cd "${ROOT}/android"
-  "${GRADLE_BIN}" :amy-service:assembleDebug
+  "${GRADLE_BIN}" :amy-service:assembleDebug :amy-service:assembleRelease
 )
 
 ADDON="${HERE}/addons/amy_android"
 mkdir -p "${ADDON}"
 cp "${ROOT}/android/amy-service/build/outputs/aar/amy-service-debug.aar" \
    "${ADDON}/amy-service-debug.aar"
+cp "${ROOT}/android/amy-service/build/outputs/aar/amy-service-release.aar" \
+   "${ADDON}/amy-service-release.aar"
 cp "${ROOT}/godot/amy.gd" "${ADDON}/amy.gd"
 cp "${ROOT}/godot/amy_android.gd" "${ADDON}/amy_android.gd"
 

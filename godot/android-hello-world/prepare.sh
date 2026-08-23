ad52d0135fa79462f1392fbdf9d9539ef29e97a2
linuxificator <linuxificator@gmail.com>
2026-08-23 16:07:45 +0200

    Prepare only debug AAR for raw-wire POC

--- godot/android-hello-world/prepare.sh
+++ godot/android-hello-world/prepare.sh
@@ -7,14 +7,12 @@ GRADLE_BIN="${GRADLE_BIN:-gradle}"
 
 (
   cd "${ROOT}/android"
-  "${GRADLE_BIN}" :amy-service:assembleDebug :amy-service:assembleRelease
+  "${GRADLE_BIN}" :amy-service:assembleDebug
 )
 
 ADDON="${HERE}/addons/amy_android"
 mkdir -p "${ADDON}"
 cp "${ROOT}/android/amy-service/build/outputs/aar/amy-service-debug.aar" \
    "${ADDON}/amy-service-debug.aar"
-cp "${ROOT}/android/amy-service/build/outputs/aar/amy-service-release.aar" \
-   "${ADDON}/amy-service-release.aar"
 
 printf 'Prepared AMY service AAR for Godot in %s\n' "${ADDON}"
